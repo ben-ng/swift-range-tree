605397ff31e250336a2c8c808d3e2f6e5e06d7f3
Ben Ng <me@benng.me>
2016-09-24 14:44:38 -0400

    Neaten up tests and hold point density constant to improve results of performance tests

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -12,19 +12,23 @@ let asc = {(a: CGPoint, b:CGPoint) -> Bool in
 }
 let constructorPerformancePoints = 1000
 let searchPerformancePoints = 10000
-let rangeSize: (UInt32, UInt32) = (1000, 1000)
-let searchRangeSize = CGSize(width: 200.0, height: 200.0)
+
+// This figures out the right range so that there's approximately one value in every four units of space
+let targetResultsPerQuery = 10
+let searchRangeWidth = UInt32(sqrt(Double(targetResultsPerQuery)))
+let rangeWidth = UInt32(sqrt(Double(searchPerformancePoints)))
+let searchRangeOriginMax = rangeWidth - searchRangeWidth
 
 func generateTwoDimensionalPoints(_ count: Int) -> [CGPoint] {
-    return (0..<count).map() { _ in CGPoint(x: Double(arc4random_uniform(rangeSize.0)), y: Double(arc4random_uniform(rangeSize.1))) }
+    return (0..<count).map() { _ in CGPoint(x: Double(arc4random_uniform(rangeWidth)), y: Double(arc4random_uniform(rangeWidth))) }
 }
 
 func createFilter(searchRangeOrigin origin: CGPoint) -> ((CGPoint) -> Bool) {
     return { (e: CGPoint) -> Bool in
         let x = e.positionIn(dimension: 0)
         let y = e.positionIn(dimension: 1)
-        return x >= origin.x && x <= origin.x + searchRangeSize.width &&
-            y >= origin.y && y <= origin.y + searchRangeSize.height
+        return x >= origin.x && x <= origin.x + CGFloat(searchRangeWidth) &&
+            y >= origin.y && y <= origin.y + CGFloat(searchRangeWidth)
     }
 }
 
@@ -32,15 +36,15 @@ class Tests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        assert(searchRangeWidth <= rangeWidth, "The search range cannot be larger than the range")
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testOneDimensionalRangeTree() {
+    func test1DRangeTree() {
         let a = RangeTree<Double>(values: [8, 1, 10, 4, 2, 7, 5, 3, 9, 6])
         XCTAssertEqual(a.description, "1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0")
         XCTAssertEqual(a.valuesInRange(rangePerDimension: (3.0, 6.0)), [3.0, 4.0, 5.0, 6.0])
@@ -51,13 +55,13 @@ class Tests: XCTestCase {
         XCTAssertEqual(a.valuesInRange(rangePerDimension: (3.0, 6.0)), [4.0, 4.5, 6.0])
     }
     
-    func testOneDimensionalRangeTreeWithDuplicates() {
+    func test1DRangeTreeWithDuplicates() {
         let a = RangeTree<Double>(values: [8, 3, 1, 3, 4, 6])
         a.insert(3)
         XCTAssertEqual(a.valuesInRange(rangePerDimension: (3.0, 6.0)), [3.0, 3.0, 3.0, 4.0, 6.0])
     }
     
-    func testTwoDimensionalRangeTree() {
+    func test2DRangeTree() {
         let twoDimFixtures = [
             CGPoint(x: 8, y: 6),
             CGPoint(x: 1, y: 9),
@@ -87,26 +91,26 @@ class Tests: XCTestCase {
         XCTAssertEqual(b.valuesInRange(rangePerDimension: (1.0, 3.0), (5.0, 8.0)).sorted(by: asc), [CGPoint(x: 2.2, y: 5.1), CGPoint(x: 2.2, y: 5.3)].sorted(by: asc))
     }
     
-    func testTwoDimensionalRangeTreeConstructorPerformance() {
+    func test2DConstructorPerformance() {
         self.measure() {
             let values = generateTwoDimensionalPoints(constructorPerformancePoints)
             _ = RangeTree<CGPoint>(values: values)
         }
     }
     
-    func testTwoDimensionalRangeTreeSearchPerformance() {
+    func test2DSearchPerformance() {
         let values = generateTwoDimensionalPoints(searchPerformancePoints)
         let a = RangeTree<CGPoint>(values: values)
         self.measure() {
-            let origin = CGPoint(x: CGFloat(arc4random_uniform(750)), y: CGFloat(arc4random_uniform(750)))
-            _ = a.valuesInRange(rangePerDimension: (origin.x, origin.x + searchRangeSize.width), (origin.y, origin.y + searchRangeSize.height))
+            let origin = CGPoint(x: CGFloat(arc4random_uniform(searchRangeOriginMax)), y: CGFloat(arc4random_uniform(searchRangeOriginMax)))
+            _ = a.valuesInRange(rangePerDimension: (origin.x, origin.x + CGFloat(searchRangeWidth)), (origin.y, origin.y + CGFloat(searchRangeWidth)))
         }
     }
     
-    func testTwoDimensionalNaiveSearchPerformance() {
+    func test2DNaiveSearchPerformance() {
         let values = generateTwoDimensionalPoints(searchPerformancePoints)
         self.measure() {
-            let origin = CGPoint(x: CGFloat(arc4random_uniform(750)), y: CGFloat(arc4random_uniform(750)))
+            let origin = CGPoint(x: CGFloat(arc4random_uniform(searchRangeOriginMax)), y: CGFloat(arc4random_uniform(searchRangeOriginMax)))
             _ = values.filter(createFilter(searchRangeOrigin: origin))
         }
     }
@@ -115,12 +119,12 @@ class Tests: XCTestCase {
 //    I'm going to leave it commented out. It's been useful for
 //    catching edge-casey bugs.
 //    
-//    func testTwoDimensionalRangeTreeSearchFuzz() {
+//    func testFuzz() {
 //        for _ in 0..<100 {
 //            let values = generateTwoDimensionalPoints(searchPerformancePoints)
 //            let a = RangeTree<CGPoint>(values: values)
-//            let origin = CGPoint(x: CGFloat(arc4random_uniform(750)), y: CGFloat(arc4random_uniform(750)))
-//            let actual = a.valuesInRange(rangePerDimension: (origin.x, origin.x + searchRangeSize.width), (origin.y, origin.y + searchRangeSize.height))
+//            let origin = CGPoint(x: CGFloat(arc4random_uniform(searchRangeOriginMax)), y: CGFloat(arc4random_uniform(searchRangeOriginMax)))
+//            let actual = a.valuesInRange(rangePerDimension: (origin.x, origin.x + CGFloat(searchRangeWidth)), (origin.y, origin.y + CGFloat(searchRangeWidth)))
 //            let expected = values.filter(createFilter(searchRangeOrigin: origin))
 //            XCTAssertEqual(actual.sorted(by: asc), expected.sorted(by: asc))
 //        }
